c57f5cc612248373322775468f01d3aecadf9e10
Ian Luis Pérez Illescas <120614784+P-Illescas-I-L@users.noreply.github.com>
2026-01-18 23:42:12 -0600

    Add files via upload

---
+++ DDT_PL.js
@@ -0,0 +1,20 @@
+const Horas= [
+    {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:00")},
+    {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:30")},
+    {'entrada': new Date("2026-01-02 00:00"), 'salida' : new Date("2026-01-02 00:59")},
+    {'entrada': new Date("2026-01-03 00:01"), 'salida' : new Date("2026-01-03 01:00")},
+    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
+    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
+    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
+
+];
+
+function substraction(){
+    const resultado = Horas.entrada - Horas.salida;
+}
+
+function mainloop(){
+    Horas.forEach((tiempo) => {
+
+    })
+}
