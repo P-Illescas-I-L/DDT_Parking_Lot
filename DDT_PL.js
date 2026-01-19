2f437205c410d80a0e394a4f59b8f8f0662b7f05
Ian Luis Pérez Illescas <iperezi1800@alumno.ipn.mx>
2026-01-19 00:49:55 -0600

    Corrected array

--- DDT_PL.js
+++ DDT_PL.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const Horas= [
-    {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:00")},
-    {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:30")},
-    {'entrada': new Date("2026-01-02 00:00"), 'salida' : new Date("2026-01-02 00:59")},
-    {'entrada': new Date("2026-01-03 00:01"), 'salida' : new Date("2026-01-03 01:00")},
-    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
-    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
-    {'entrada': new Date("2026-01-04 00:01"), 'salida' : new Date("2026-01-04 01:01")},
-
-];
-
-function substraction(){
-    const resultado = Horas.entrada - Horas.salida;
-}
-
-function mainloop(){
-    Horas.forEach((tiempo) => {
-
-    })
-=======
 const ValetParking= [
     {'entrada': new Date("2026-01-01 00:00"), 'salida' : new Date("2026-01-01 01:00")},
     {'entrada': new Date("2026-01-02 00:00"), 'salida' : new Date("2026-01-02 01:30")},
@@ -49,5 +28,4 @@ function mainloop(){
     ValetParking.forEach((tiempo) => {
 
     })
->>>>>>> 7e4a5cf (Added correted hours)
 }
