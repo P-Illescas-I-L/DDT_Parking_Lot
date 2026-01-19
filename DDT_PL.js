a2e0f27badced62e7fcfd72ed49acf01be6c339d
Ian Luis Pérez Illescas <iperezi1800@alumno.ipn.mx>
2026-01-19 00:29:47 -0600

    Added correted hours

--- DDT_PL.js
+++ DDT_PL.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const Horas= [
     {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:00")},
     {'entrada': new Date("2026-01-01 00:01"), 'salida' : new Date("2026-01-01 00:30")},
@@ -17,4 +18,36 @@ function mainloop(){
     Horas.forEach((tiempo) => {
 
     })
+=======
+const ValetParking= [
+    {'entrada': new Date("2026-01-01 00:00"), 'salida' : new Date("2026-01-01 01:00")},
+    {'entrada': new Date("2026-01-02 00:00"), 'salida' : new Date("2026-01-02 01:30")},
+    {'entrada': new Date("2026-01-03 00:00"), 'salida' : new Date("2026-01-03 00:59")},
+    {'entrada': new Date("2026-01-04 00:00"), 'salida' : new Date("2026-01-04 01:00")},
+    {'entrada': new Date("2026-01-05 00:00"), 'salida' : new Date("2026-01-05 01:01")},
+    {'entrada': new Date("2026-01-06 00:00"), 'salida' : new Date("2026-01-06 04:59")},
+    {'entrada': new Date("2026-01-07 00:00"), 'salida' : new Date("2026-01-07 05:00")},
+    {'entrada': new Date("2026-01-08 00:00"), 'salida' : new Date("2026-01-08 05:01")},
+    {'entrada': new Date("2026-01-09 00:00"), 'salida' : new Date("2026-01-09 06:00")},
+    {'entrada': new Date("2026-01-10 00:00"), 'salida' : new Date("2026-01-10 23:59")},
+    {'entrada': new Date("2026-01-11 00:00"), 'salida' : new Date("2026-01-11 00:00")},
+    {'entrada': new Date("2026-01-12 00:00"), 'salida' : new Date("2026-01-13 00:01")},
+];
+
+function substraction(){
+    const resultado = ValetParking.entrada - ValetParking.salida;
+    const perDay = 18;
+    const fiveHrsOrLess = 12;
+    if(resultado >= 0 && resultado == 5){
+        console.log("Your tariff is: ", fiveHrsOrLess);
+    } else {
+        console.log("Your tariff is: ", perDay);
+    }
+}
+
+function mainloop(){
+    ValetParking.forEach((tiempo) => {
+
+    })
+>>>>>>> 7e4a5cf (Added correted hours)
 }
